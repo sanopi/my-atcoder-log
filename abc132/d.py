8f680930728cba35b6f98513969c1069793fb8cb
tomohiro.sano <36667265+sanopi@users.noreply.github.com>
2019-06-29 23:15:45 +0900

    abc132 modified d

--- abc132/d.py
+++ abc132/d.py
@@ -1,6 +1,5 @@
 # 不正解のコード
-from operator import mul
-from functools import reduce
+from math import factorial
 
 
 def main():
@@ -10,11 +9,11 @@ def main():
     k = int(inp[1])
     r = n - k
     for i in range(1, k + 1):
-        if r + 2 < i:
-            print('0')
+        if r + 1 < i:
+            print(0)
             continue
-        elif r + 1 < i:
-            rr = r
+        elif r + 1 == i:
+            rr = 1
         else:
             rr = cmb(r + 1, i)
 
@@ -24,16 +23,11 @@ def main():
             bb = 1
         else:
             bb = cmb(k - 1, i - 1)
-        print((rr % MOD) * (bb % MOD) % MOD)
+        print(((rr % MOD) * (bb % MOD)) % MOD)
 
 
 def cmb(n, r):
-    r = min(n-r, r)
-    if r == 0:
-        return 1
-    over = reduce(mul, range(n, n - r, -1))
-    under = reduce(mul, range(1, r + 1))
-    return over // under
+    return factorial(n) // (factorial(r) * factorial(n - r))
 
 
 if __name__ == '__main__':
