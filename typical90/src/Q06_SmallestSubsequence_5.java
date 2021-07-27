7ed9545748ec1fc6b4660ddb7eeec522764437f3
sanopi <36667265+sanopi@users.noreply.github.com>
2021-07-27 15:22:10 +0900

    typical6_SmallestSubsequence AC

--- typical90/src/Q06_SmallestSubsequence_5.java
+++ typical90/src/Q06_SmallestSubsequence_5.java
@@ -1,6 +1,5 @@
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Q06_SmallestSubsequence_5 {
@@ -8,23 +7,34 @@ public class Q06_SmallestSubsequence_5 {
     public static void main(String[] args) {
         int n = nextInt();
         int k = nextInt();
-        List<Character> c = new ArrayList<>(k);
-        next().chars().forEach(ch -> c.add((char)ch));
+        char[] s = next().toCharArray();
 
-        List<Character> ans = c.subList(0, k);
-        for (int i = k; i < n; i++) {
-            ans.add(c.get(i));
-            for (int j = 0; j < k; j++) {
-                if (ans.get(j) > ans.get(j+1)) {
-                    ans.remove(j);
-                    break;
+        int[][] c = new int[n+1][26];
+        Arrays.fill(c[n], n);
+        for (int i = n-1; i >= 0; i--) {
+            int si = s[i] - 'a';
+            for (int j = 0; j < 26; j++) {
+                if (j == si) {
+                    c[i][j] = i;
+                } else {
+                    c[i][j] = c[i+1][j];
                 }
             }
-            if (ans.size() > k) {
-                ans.remove(ans.size()-1);
+        }
+
+        StringBuilder sb = new StringBuilder(k);
+        for (int i = 0; i < n && k > 0;) {
+            for (int j = 0; j < c[i].length; j++) {
+                if (c[i][j] > n-k) {
+                    continue;
+                }
+                sb.append((char) (j + 'a'));
+                k -= 1;
+                i = c[i][j] + 1;
+                break;
             }
         }
-        out.println(ans.stream().map(String::valueOf).reduce((s1, s2) -> s1 + s2).get());
+        out.println(sb.toString());
         out.flush();
     }
 
