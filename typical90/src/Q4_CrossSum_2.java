d83724a40901a8bcbac4737a08cd8402608a14a2
sanopi <36667265+sanopi@users.noreply.github.com>
2021-07-27 12:31:20 +0900

    typical4_CrossSum

---
+++ typical90/src/Q4_CrossSum_2.java
@@ -0,0 +1,45 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Q4_CrossSum_2 {
+
+    public static void main(String[] args) {
+        PrintWriter out = new PrintWriter(System.out);
+        int h = nextInt();
+        int w = nextInt();
+
+        int[][] a = new int[h][w];
+
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                a[i][j] = nextInt();
+            }
+        }
+
+        int[] hSum = new int[h];
+        int[] wSum = new int[w];
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                hSum[i] += a[i][j];
+                wSum[j] += a[i][j];
+            }
+        }
+
+        for (int i = 0; i < h; i++) {
+            String[] ans = new String[w];
+            for (int j = 0; j < w; j++) {
+                ans[j] = String.valueOf(hSum[i] + wSum[j] - a[i][j]);
+            }
+            out.println(String.join(" ", ans));
+        }
+        out.flush();
+    }
+
+    static Scanner scanner = new Scanner(System.in);
+    static int nextInt() {
+        return Integer.parseInt(scanner.next());
+    }
+    static long nextLong() {
+        return Long.parseLong(scanner.next());
+    }
+}
