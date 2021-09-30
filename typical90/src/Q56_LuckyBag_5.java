f2a836250f082804b671ae6f0ad2e2f5cf3d6d90
sanopi <36667265+sanopi@users.noreply.github.com>
2021-09-30 09:25:41 +0900

    typical56_LuckyBag | AC

--- typical90/src/Q56_LuckyBag_5.java
+++ typical90/src/Q56_LuckyBag_5.java
@@ -1,40 +1,63 @@
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
-
-import static java.lang.Math.min;
-import static java.util.Arrays.fill;
 
 public class Q56_LuckyBag_5 {
 
     public static void main(String[] args) {
         int n = nextInt();
         int s = nextInt();
-
-        String[][] dp = new String[n+1][s+1];
-        for (int i = 0; i <= n; i++) {
-            fill(dp[i], "X");
+        int[] a = new int[n];
+        int[] b = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = nextInt();
+            b[i] = nextInt();
         }
-        dp[0][0] = "";
-        for (int i = 1; i <= n; i++) {
-            int a = nextInt();
-            int b = nextInt();
-            for (int j = s - min(a, b); j >= 0 ; j--) {
-                if (dp[i-1][j].length() != i-1) {
-                    continue;
+
+        boolean[][] dp = new boolean[n+1][s+1];
+        dp[0][0] = true;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= s; j++) {
+                if (!dp[i][j]) { continue; }
+                if (j+a[i] <= s) {
+                    dp[i+1][j+a[i]] = true;
                 }
-                if (dp[i-1][j].equals("X")) {
-                    continue;
-                }
-                if (j+a <= s) {
-                    dp[i][j+a] = dp[i-1][j] + "A";
-                }
-                if (j+b <= s) {
-                    dp[i][j+b] = dp[i-1][j] + "B";
+                if (j+b[i] <= s) {
+                    dp[i+1][j+b[i]] = true;
                 }
             }
         }
 
-        out.println(dp[n][s].equals("X") ? "Impossible" : dp[n][s]);
+        if (!dp[n][s]) {
+            System.out.println("Impossible");
+            return;
+        }
+
+        int yen = s;
+        List<String> ansRev = new ArrayList<>();
+        for (int i = n-1; i >= 0; i--) {
+            if (yen-a[i] >= 0) {
+                if (dp[i][yen-a[i]]) {
+                    yen = yen-a[i];
+                    ansRev.add("A");
+                    continue;
+                }
+            }
+            if (yen-b[i] >= 0) {
+                if (dp[i][yen-b[i]]) {
+                    yen = yen-b[i];
+                    ansRev.add("B");
+                    continue;
+                }
+            }
+            System.out.println("Impossible");
+            return;
+        }
+
+        Collections.reverse(ansRev);
+        out.println(String.join("", ansRev));
         out.flush();
     }
 
