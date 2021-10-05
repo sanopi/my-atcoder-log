b7e69e2775a67deed1c873ec126680f7381ef194
sanopi <36667265+sanopi@users.noreply.github.com>
2021-10-05 23:41:56 +0900

    ABC221C 速い版

--- abc221/src/ABC221C.java
+++ abc221/src/ABC221C.java
@@ -5,28 +5,51 @@ import java.util.Scanner;
 public class ABC221C {
 
     public static void main(String[] args) {
+//        char[] n = next().toCharArray();
+//        Arrays.sort(n);
+//
+//        int max = 0;
+//        for (int i = 0; i < (1 << n.length); i++) {
+//            StringBuilder a = new StringBuilder();
+//            StringBuilder b = new StringBuilder();
+//            for (int j = 0; j < n.length; j++) {
+//                if ((i & (1 << j)) == 0) {
+//                    a.append(n[n.length - j - 1]);
+//                } else {
+//                    b.append(n[n.length - j - 1]);
+//                }
+//            }
+//            String as = a.toString();
+//            String bs = b.toString();
+//            if (as.length() > 0 && bs.length() > 0) {
+//                max = Math.max(max, Integer.parseInt(as) * Integer.parseInt(bs));
+//            }
+//        }
+//
+//        out.println(max);
+//        out.flush();
+        solve();
+    }
+
+    private static void solve() {
         char[] n = next().toCharArray();
         Arrays.sort(n);
-
-        int max = 0;
-        for (int i = 0; i < (1 << n.length); i++) {
-            StringBuilder a = new StringBuilder();
-            StringBuilder b = new StringBuilder();
-            for (int j = 0; j < n.length; j++) {
-                if ((i & (1 << j)) == 0) {
-                    a.append(n[n.length - j - 1]);
-                } else {
-                    b.append(n[n.length - j - 1]);
-                }
-            }
-            String as = a.toString();
-            String bs = b.toString();
-            if (as.length() > 0 && bs.length() > 0) {
-                max = Math.max(max, Integer.parseInt(as) * Integer.parseInt(bs));
+        long a = n[n.length-1]-'0';
+        long b = n[n.length-2]-'0';
+        if (n.length == 2) {
+            out.println(a * b);
+            out.flush();
+            return;
+        }
+        for (int i = n.length - 3; i >= 0; i--) {
+            if ((10 * a + n[i]) * b > (10 * b + n[i]) * a) {
+                a = 10 * a + n[i]-'0';
+            } else {
+                b = 10 * b + n[i]-'0';
             }
         }
 
-        out.println(max);
+        out.println(a * b);
         out.flush();
     }
 
