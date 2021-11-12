ff4c77ed43f2987ca4da72dab8b82f3f9b22ceae
sanopi <36667265+sanopi@users.noreply.github.com>
2021-11-12 19:19:46 +0900

    ABC206D

---
+++ abc206/src/ABC206D.java
@@ -0,0 +1,116 @@
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
+public class ABC206D {
+
+    public static void main(String[] args) {
+        int n = nextInt();
+        int[] a = nextIntArray(n);
+        int max = Arrays.stream(a).max().orElseThrow();
+        UnionFind uf = new UnionFind(max+1);
+        for (int i = 0; i < n/2; i++) {
+            uf.unite(a[i], a[n-1-i]);
+        }
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+        for (int i = 0; i < max+1; i++) {
+            int p = uf.find(i);
+            if (uf.size(p) > 1) {
+                Set<Integer> set = map.getOrDefault(p, new HashSet<>());
+                set.add(i);
+                map.put(p, set);
+            }
+        }
+//        for (int i = 0; i < n; i++) {
+//            int p = uf.find(a[i]);
+//            Set<Integer> set = map.getOrDefault(p, new HashSet<>());
+//            set.add(a[i]);
+//            map.put(p, set);
+//        }
+
+        int ans = map.values().stream()
+            .mapToInt(Set::size)
+            .map(i -> i - 1)
+            .sum();
+        out.println(ans);
+        out.flush();
+    }
+
+    static PrintWriter out = new PrintWriter(System.out);
+    static Scanner scanner = new Scanner(System.in);
+    static String next() { return scanner.next(); }
+    static int nextInt() { return Integer.parseInt(next()); }
+    static long nextLong() { return Long.parseLong(next()); }
+    static double nextDouble() { return Double.parseDouble(next()); }
+    static int[] nextIntArray(int n) {
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) { array[i] = nextInt(); }
+        return array;
+    }
+    static long[] nextLongArray(int n) {
+        long[] array = new long[n];
+        for (int i = 0; i < n; i++) { array[i] = nextLong(); }
+        return array;
+    }
+
+    private static class UnionFind {
+        int[] parents; // 親（根）の情報を持つ
+        int[] ranks; // 深さの最大値の情報を持つ（複雑度と同じくらいに考えておく。）
+        int[] sizes;
+
+        UnionFind(int n) {
+            parents = new int[n];
+            ranks = new int[n];
+            for (int i = 0; i < n; i++) {
+                parents[i] = i;
+                ranks[i] = 0;
+            }
+            sizes = new int[n];
+            Arrays.fill(sizes, 1);
+        }
+
+        private int find(int x) {
+            int parent = parents[x];
+            if (x == parent) {
+                return x;
+            }
+            parents[x] = find(parent); // 圧縮（木の繋ぎ直し）
+            return parents[x];
+        }
+
+        private boolean same(int x, int y) {
+            return find(x) == find(y);
+        }
+
+        private int size(int x) {
+            return sizes[find(x)];
+        }
+
+        private void unite(int x, int y) {
+            int xParent = find(x);
+            int yParent = find(y);
+            if (xParent == yParent) {
+                return;
+            }
+
+            int xRank = ranks[xParent];
+            int yRank = ranks[yParent];
+            if (xRank < yRank) {
+                parents[xParent] = yParent;
+                sizes[yParent] += sizes[xParent];
+            } else if (yRank < xRank) {
+                parents[yParent] = xParent;
+                sizes[xParent] += sizes[yParent];
+            } else { // xRank == yRank
+                parents[xParent] = yParent;
+                ranks[xParent]++;
+                sizes[yParent] += sizes[xParent];
+            }
+        }
+    }
+
+}
