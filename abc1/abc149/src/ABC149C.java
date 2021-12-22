fd26502ee5afe95778315c8e67f9cc649f6b6e27
sanopi <36667265+sanopi@users.noreply.github.com>
2021-12-22 22:56:41 +0900

    ABC149C

---
+++ abc1/abc149/src/ABC149C.java
@@ -0,0 +1,51 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class ABC149C {
+
+    public static void main(String[] args) {
+        int x = nextInt();
+        if (x == 2) {
+            System.out.println(2);
+            return;
+        }
+
+        while (true) {
+            if (isPrime(x)) {
+                out.println(x);
+                break;
+            }
+            x++;
+        }
+        out.flush();
+    }
+
+    private static boolean isPrime(int x) {
+        for (int i = 2; i * i <= x; i++) {
+            if (x % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
+    static PrintWriter out = new PrintWriter(System.out);
+    static Scanner scanner = new Scanner(System.in);
+    static String next() { return scanner.next(); }
+    static int nextInt() { return Integer.parseInt(next()); }
+    static long nextLong() { return Long.parseLong(next()); }
+    static double nextDouble() { return Double.parseDouble(next()); }
+    static int[] nextIntArray(int n) {
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) { array[i] = nextInt(); }
+        return array;
+    }
+    static long[] nextLongArray(int n) {
+        long[] array = new long[n];
+        for (int i = 0; i < n; i++) { array[i] = nextLong(); }
+        return array;
+    }
+
+}
