7fffd450286cf611775dec81f6fdeaa3c6322087
sanopi <36667265+sanopi@users.noreply.github.com>
2021-12-30 23:52:02 +0900

    AGC037A | dp による解放

--- agc/agc037/src/AGC037A.java
+++ agc/agc037/src/AGC037A.java
@@ -5,6 +5,23 @@ public class AGC037A {
 
     public static void main(String[] args) {
         String s = next();
+//        solve1(s);
+        solve2(s);
+        out.flush();
+    }
+    private static void solve2(final String s) {
+        int n = s.length();
+        char[] chars = s.toCharArray();
+        int[][] dp = new int[n][2];
+        dp[0][0] = 1;
+
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.max(chars[i] != chars[i - 1] ?dp[i-1][0]+1:0, dp[i-1][1]+1);
+            dp[i][1] = Math.max((i>1?dp[i-2][0]+1:0), (i>3&&!(chars[i]==chars[i-2]&&chars[i-1]==chars[i-3])?dp[i-2][1]+1:0));
+        }
+        out.println(Math.max(dp[n-1][0], dp[n-1][1]));
+    }
+    private static void solve1(final String s) {
         int n = s.length();
         char[] chars = s.toCharArray();
         int count = 1;
@@ -22,7 +39,6 @@ public class AGC037A {
             count++;
         }
         out.println(count);
-        out.flush();
     }
 
     static PrintWriter out = new PrintWriter(System.out);
