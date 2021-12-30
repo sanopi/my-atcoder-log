3ad4dd46f02a45e0980b66f5b1456387e399e05e
sanopi <36667265+sanopi@users.noreply.github.com>
2021-12-31 00:26:18 +0900

    ABC144C

---
+++ abc1/abc144/src/ABC144C.java
@@ -0,0 +1,35 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class ABC144C {
+
+    public static void main(String[] args) {
+        long n = nextLong();
+        long ans = n;
+        for (long i = 1; i*i <= n; i++) {
+            if (n%i == 0) {
+                ans = Math.min(ans, i+n/i-2);
+            }
+        }
+        out.println(ans);
+        out.flush();
+    }
+
+    static PrintWriter out = new PrintWriter(System.out);
+    static Scanner scanner = new Scanner(System.in);
+    static String next() { return scanner.next(); }
+    static int nextInt() { return Integer.parseInt(next()); }
+    static long nextLong() { return Long.parseLong(next()); }
+    static double nextDouble() { return Double.parseDouble(next()); }
+    static int[] nextIntArray(int n) {
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) { array[i] = nextInt(); }
+        return array;
+    }
+    static long[] nextLongArray(int n) {
+        long[] array = new long[n];
+        for (int i = 0; i < n; i++) { array[i] = nextLong(); }
+        return array;
+    }
+
+}
