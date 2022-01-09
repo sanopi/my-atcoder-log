8b2c57cddecdab9ea29e0496a2c8b092c37e85d5
sanopi <36667265+sanopi@users.noreply.github.com>
2022-01-09 14:10:47 +0900

    educational dp contest | Sushi

--- other/educational-dp/Q10_Sushi.java
+++ other/educational-dp/Q10_Sushi.java
@@ -3,13 +3,38 @@ import java.util.Scanner;
 
 public class Q10_Sushi {
 
+    private static int n;
+    private static double[][][] dp;
     public static void main(String[] args) {
-        int n = nextInt();
+        n = nextInt();
         int[] a = nextIntArray(n);
 
-        int[][] dp = new int[n][n];
+        dp = new double[n+1][n+1][n+1];
+        int[] counts = new int[4];
+        for(int i:a)counts[i]++;
 
+        double ans = memoRec(counts[1],counts[2],counts[3]);
+
+        out.println(ans);
         out.flush();
+    }
+
+    private static double memoRec(int one, int two, int three) {
+        if(one+two+three==0) return 0;
+        if (dp[one][two][three] > 0) {
+            return dp[one][two][three];
+        }
+        double sum = one+two+three;
+        double k = 1-(1-sum/n);
+
+        double res = 0;
+        if(one>0) res+=(memoRec(one-1, two, three)*(((double) one/n))/k);
+        if(two>0) res+=(memoRec(one+1, two-1, three)*(((double) two/n))/k);
+        if(three>0) res+=(memoRec(one, two+1, three-1)*(((double) three/n))/k);
+        res += 1/k;
+
+        dp[one][two][three]=res;
+        return res;
     }
 
     static PrintWriter out = new PrintWriter(System.out);
