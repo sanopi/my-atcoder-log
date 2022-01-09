feee9cfea4c31c12b1282e290bce02ae8e07ad48
sanopi <36667265+sanopi@users.noreply.github.com>
2022-01-09 14:41:52 +0900

    ABC056C

---
+++ abc0/abc056/ABC056C.java
@@ -0,0 +1,35 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class ABC056C {
+
+    public static void main(String[] args) {
+        int x = nextInt();
+        for (int i = 0; i <= x; i++) {
+            if (i*(i+1)/2 >= x) {
+                System.out.println(i);
+                return;
+            }
+        }
+
+        out.flush();
+    }
+
+    static PrintWriter out = new PrintWriter(System.out);
+    static Scanner scanner = new Scanner(System.in);
+    static String next() { return scanner.next(); }
+    static int nextInt() { return Integer.parseInt(next()); }
+    static long nextLong() { return Long.parseLong(next()); }
+    static double nextDouble() { return Double.parseDouble(next()); }
+    static int[] nextIntArray(int n) {
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) { array[i] = nextInt(); }
+        return array;
+    }
+    static long[] nextLongArray(int n) {
+        long[] array = new long[n];
+        for (int i = 0; i < n; i++) { array[i] = nextLong(); }
+        return array;
+    }
+
+}
