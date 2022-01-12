f003bb7b75bc5def824b066d2a31af8fcc30e095
sanopi <36667265+sanopi@users.noreply.github.com>
2022-01-12 09:43:26 +0900

    ABC072D

---
+++ abc0/abc072/ABC072D.java
@@ -0,0 +1,45 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class ABC072D {
+
+    public static void main(String[] args) {
+        int n = nextInt();
+        int[] p = new int[n];
+        for (int i = 0; i < n; i++) {
+            p[i] = nextInt()-1;
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (p[i] == i) {
+                if (i<n-1 && p[i+1]==i+1) {
+                    int tmp = p[i];
+                    p[i] = p[i+1];
+                    p[i+1] = tmp;
+                }
+                ans++;
+            }
+        }
+        out.println(ans);
+        out.flush();
+    }
+
+    static PrintWriter out = new PrintWriter(System.out);
+    static Scanner scanner = new Scanner(System.in);
+    static String next() { return scanner.next(); }
+    static int nextInt() { return Integer.parseInt(next()); }
+    static long nextLong() { return Long.parseLong(next()); }
+    static double nextDouble() { return Double.parseDouble(next()); }
+    static int[] nextIntArray(int n) {
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) { array[i] = nextInt(); }
+        return array;
+    }
+    static long[] nextLongArray(int n) {
+        long[] array = new long[n];
+        for (int i = 0; i < n; i++) { array[i] = nextLong(); }
+        return array;
+    }
+
+}
