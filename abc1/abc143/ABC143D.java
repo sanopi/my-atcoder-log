4b5d79217eba13d2ac632ad1a6ac246934c95f1c
sanopi <36667265+sanopi@users.noreply.github.com>
2022-01-22 00:18:30 +0900

    ABC143D | 全探索

--- abc1/abc143/ABC143D.java
+++ abc1/abc143/ABC143D.java
@@ -8,21 +8,40 @@ public class ABC143D {
         int n = nextInt();
         int[] l = nextIntArray(n);
         Arrays.sort(l);
+//        long ans = solve1(n, l);
+        long ans = solve2(n, l);
+        out.println(ans);
+        out.flush();
+    }
+    private static long solve2(int n, int[] l) {
         long ans = 0;
         for (int a = 0; a < n - 2; a++) {
             for (int b = a+1; b < n - 1; b++) {
-                if (l[a]+l[b]<=l[b+1]) {
+                for (int c = b+1; c < n; c++) {
+                    if (l[a]+l[b]>l[c]) {
+                        ans++;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+    private static long solve1(int n, int[] l) {
+        long ans = 0;
+        for (int a = 0; a < n - 2; a++) {
+            for (int b = a+1; b < n - 1; b++) {
+                if (l[a]+ l[b]<= l[b+1]) {
                     continue;
                 }
-                if (l[a]+l[b]>l[n-1]) {
-                    ans += (n-1) - b;
+                if (l[a]+ l[b]> l[n -1]) {
+                    ans += (n -1) - b;
                     continue;
                 }
                 int ok = b+1;
-                int ng = n-1;
+                int ng = n -1;
                 while (Math.abs(ok-ng) > 1) {
                     int p = (ok+ng)/2;
-                    if (l[a]+l[b]>l[p]) {
+                    if (l[a]+ l[b]> l[p]) {
                         ok = p;
                     } else {
                         ng = p;
@@ -31,8 +50,7 @@ public class ABC143D {
                 ans+=(ok-b);
             }
         }
-        out.println(ans);
-        out.flush();
+        return ans;
     }
 
     static PrintWriter out = new PrintWriter(System.out);
