6df292e32eab00a10353aa79ce10581c0f0b72df
sanopi <36667265+sanopi@users.noreply.github.com>
2022-01-31 09:50:09 +0900

    ABC237E | ポテンシャル法を用いた正しい回答

--- abc2/abc23/abc237/ABC237E.java
+++ abc2/abc23/abc237/ABC237E.java
@@ -24,6 +24,40 @@ public class ABC237E {
             g[v].add(u);
         }
 
+//        out.println(solve1(n, h, g));
+        out.println(solve2(n, h, g));
+        out.flush();
+    }
+    private static long solve2(int n, int[] h, ArrayList<Integer>[] g) {
+        long[] cost = new long[n];
+        Arrays.fill(cost, Long.MAX_VALUE);
+        cost[0] = 0;
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.comparing(i -> cost[i]));
+        pq.add(0);
+        while (!pq.isEmpty()) {
+            int current = pq.poll();
+            for (Integer next : g[current]) {
+                int diff = h[current] - h[next];
+                long nextCost = cost[current] + Math.max(0, -diff);
+                if (cost[next] <= nextCost) {
+                    continue;
+                }
+                cost[next] = nextCost;
+                pq.add(next);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            cost[i] += h[i];
+        }
+        return h[0]-Arrays.stream(cost).min().getAsLong();
+    }
+
+
+    /**
+     * TLEになる嘘解法
+     */
+    private static long solve1(int n, int[] h, ArrayList<Integer>[] g) {
         long[] fun = new long[n];
         Arrays.fill(fun, Long.MIN_VALUE);
         fun[0] = 0;
@@ -46,8 +80,7 @@ public class ABC237E {
                 }
             }
         }
-        System.out.println(Arrays.stream(fun).max().getAsLong());
-        out.flush();
+        return Arrays.stream(fun).max().getAsLong();
     }
 
 
