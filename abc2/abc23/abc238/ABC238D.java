6e5b0e4ec7c6fc76c352d1a118c649f1fd5df661
sanopi <36667265+sanopi@users.noreply.github.com>
2022-02-05 23:15:58 +0900

    ABC238D | 想定解

--- abc2/abc23/abc238/ABC238D.java
+++ abc2/abc23/abc238/ABC238D.java
@@ -1,5 +1,4 @@
 import java.io.PrintWriter;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class ABC238D {
@@ -9,43 +8,8 @@ public class ABC238D {
         for (int i = 0; i < t; i++) {
             long a = nextLong();
             long s = nextLong();
-
-            boolean[] isZero = new boolean[61];
-            Arrays.fill(isZero, true);
-            long aa = a;
-            int j = 0;
-            while (aa > 0) {
-                if (aa % 2 != 0) {
-                    isZero[j] = false;
-                }
-                aa /= 2;
-                j++;
-            }
-
-            long diff = s-a-a;
-            if (diff < 0) {
-                out.println("No");
-                continue;
-            }
-            if (diff == 0) {
-                out.println("Yes");
-                continue;
-            }
-            for (int k = 60; k >= 0; k--) {
-                long l = 1L << k;
-                if (isZero[k] && l <= diff) {
-                    diff -= l;
-                }
-                if (diff == 0) {
-                    break;
-                }
-            }
-            if (diff == 0) {
-                out.println("Yes");
-            } else {
-                out.println("No");
-            }
-
+            long saa = s - a - a;
+            out.println(saa>=0 && (saa &a)==0 ? "Yes" : "No");
         }
 
         out.flush();
