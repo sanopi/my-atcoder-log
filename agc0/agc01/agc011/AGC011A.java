864f15f971d9a3057454a68c3d2721fd521daab1
sanopi <36667265+sanopi@users.noreply.github.com>
2022-02-10 00:01:05 +0900

    名前修正

--- agc0/agc01/agc011/AGC011A.java
+++ agc0/agc01/agc011/AGC011A.java
@@ -2,7 +2,7 @@ import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class ABC011A {
+public class AGC011A {
 
     public static void main(String[] args) {
         int n = nextInt();
