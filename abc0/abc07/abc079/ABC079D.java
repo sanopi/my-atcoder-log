89c29231795a41cc68bb326e09d1d58ab4fc08bd
sanopi <36667265+sanopi@users.noreply.github.com>
2022-02-18 09:55:22 +0900

    ABC079D

---
+++ abc0/abc07/abc079/ABC079D.java
@@ -0,0 +1,76 @@
+import java.io.PrintWriter;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class ABC079D {
+
+    public static void main(String[] args) {
+        int h = nextInt();
+        int w = nextInt();
+        int[][] c = new int[10][10];
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                c[i][j] = nextInt();
+            }
+        }
+
+        int[] cost = new int[10];
+        for (int i = 0; i < 10; i++) {
+            boolean[] done = new boolean[10];
+            PriorityQueue<Pair> pq = new PriorityQueue<>(Comparator.comparing(p -> p.cost));
+            pq.add(new Pair(i, 0));
+            while (!pq.isEmpty()) {
+                Pair current = pq.poll();
+                if (current.i == 1) {
+                    cost[i]=current.cost;
+                    break;
+                }
+                done[current.i]=true;
+                for (int j = 0; j < 10; j++) {
+                    if (done[j]) continue;
+                    pq.add(new Pair(j, current.cost + c[current.i][j]));
+                }
+            }
+        }
+
+        long ans = 0;
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                int a = nextInt();
+                ans += (a>=0?cost[a]:0);
+            }
+        }
+        out.println(ans);
+
+
+        out.flush();
+    }
+
+    private static class Pair {
+        int i;
+        int cost;
+        public Pair(int i, int cost) {
+            this.i = i;
+            this.cost = cost;
+        }
+    }
+
+    static PrintWriter out = new PrintWriter(System.out);
+    static Scanner scanner = new Scanner(System.in);
+    static String next() { return scanner.next(); }
+    static int nextInt() { return Integer.parseInt(next()); }
+    static long nextLong() { return Long.parseLong(next()); }
+    static double nextDouble() { return Double.parseDouble(next()); }
+    static int[] nextIntArray(int n) {
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) { array[i] = nextInt(); }
+        return array;
+    }
+    static long[] nextLongArray(int n) {
+        long[] array = new long[n];
+        for (int i = 0; i < n; i++) { array[i] = nextLong(); }
+        return array;
+    }
+
+}
