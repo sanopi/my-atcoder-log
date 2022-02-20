493844ce33727b6b35afb5f42c456231e927b45b
sanopi <36667265+sanopi@users.noreply.github.com>
2022-02-20 11:01:47 +0900

    AGC005A | Dequeを使用（想定解）

--- agc0/agc00/agc005/AGC005A.java
+++ agc0/agc00/agc005/AGC005A.java
@@ -1,10 +1,38 @@
 import java.io.PrintWriter;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Scanner;
 
 public class AGC005A {
 
     public static void main(String[] args) {
         String x = next();
+//        solve1(x);
+        solve2(x);
+        out.flush();
+    }
+
+    private static void solve2(String x) {
+        Deque<Character> deque = new ArrayDeque<>();
+        for (int i = 0; i < x.length(); i++) {
+            char c = x.charAt(i);
+            if (c =='S') {
+                deque.addLast(c);
+                continue;
+            }
+            if (c == 'T') {
+                Character last = deque.peekLast();
+                if (last != null && last == 'S') {
+                    deque.pollLast();
+                } else {
+                    deque.addLast(c);
+                }
+            }
+        }
+        out.println(deque.size());
+    }
+
+    private static void solve1(String x) {
         int t = 0;
         int max = 0;
         for (int i = 0; i < x.length(); i++) {
@@ -16,7 +44,6 @@ public class AGC005A {
             max = Math.max(max, t);
         }
         out.println(max*2);
-        out.flush();
     }
 
     static PrintWriter out = new PrintWriter(System.out);
