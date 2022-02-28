594569cd76bc8f432e8408f4d85399c125982e0c
sanopi <36667265+sanopi@users.noreply.github.com>
2022-02-28 09:37:28 +0900

    ABC075C

---
+++ abc0/abc07/abc075/ABC075C.java
@@ -0,0 +1,108 @@
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ABC075C {
+
+    public static void main(String[] args) {
+        int n = nextInt();
+        int m = nextInt();
+        int[] a = new int[m];
+        int[] b = new int[m];
+        for (int i = 0; i < m; i++) {
+            a[i] = nextInt()-1;
+            b[i] = nextInt()-1;
+        }
+
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            UnionFind uf = new UnionFind(n);
+            for (int j = 0; j < m; j++) {
+                if (i==j) continue;
+                uf.unite(a[j], b[j]);
+            }
+            for (int j = 1; j < n; j++) {
+                if (!uf.same(j, j - 1)) {
+                    count++;
+                    break;
+                }
+            }
+        }
+        out.println(count);
+        out.flush();
+    }
+
+    static PrintWriter out = new PrintWriter(System.out);
+    static Scanner scanner = new Scanner(System.in);
+    static String next() { return scanner.next(); }
+    static int nextInt() { return Integer.parseInt(next()); }
+    static long nextLong() { return Long.parseLong(next()); }
+    static double nextDouble() { return Double.parseDouble(next()); }
+    static int[] nextIntArray(int n) {
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) { array[i] = nextInt(); }
+        return array;
+    }
+    static long[] nextLongArray(int n) {
+        long[] array = new long[n];
+        for (int i = 0; i < n; i++) { array[i] = nextLong(); }
+        return array;
+    }
+
+    private static class UnionFind {
+        int[] parents; // 親（根）の情報を持つ
+        int[] ranks; // 深さの最大値の情報を持つ（複雑度と同じくらいに考えておく。）
+        int[] sizes;
+
+        UnionFind(int n) {
+            parents = new int[n];
+            ranks = new int[n];
+            for (int i = 0; i < n; i++) {
+                parents[i] = i;
+                ranks[i] = 0;
+            }
+            sizes = new int[n];
+            Arrays.fill(sizes, 1);
+        }
+
+        private int find(int x) {
+            int parent = parents[x];
+            if (x == parent) {
+                return x;
+            }
+            parents[x] = find(parent); // 圧縮（木の繋ぎ直し）
+            return parents[x];
+        }
+
+        private boolean same(int x, int y) {
+            return find(x) == find(y);
+        }
+
+        private int size(int x) {
+            return sizes[find(x)];
+        }
+
+        private void unite(int x, int y) {
+            int xParent = find(x);
+            int yParent = find(y);
+            if (xParent == yParent) {
+                return;
+            }
+
+            int xRank = ranks[xParent];
+            int yRank = ranks[yParent];
+            if (xRank < yRank) {
+                parents[xParent] = yParent;
+                sizes[yParent] += sizes[xParent];
+            } else if (yRank < xRank) {
+                parents[yParent] = xParent;
+                sizes[xParent] += sizes[yParent];
+            } else { // xRank == yRank
+                parents[xParent] = yParent;
+                ranks[xParent]++;
+                sizes[yParent] += sizes[xParent];
+            }
+        }
+    }
+
+}
