c4a27deb6fb1a9b308212140863b1824e3862409
sanopi <36667265+sanopi@users.noreply.github.com>
2022-03-04 19:57:32 +0900

    ABC090D

---
+++ abc0/abc09/abc090/ABC090D.java
@@ -0,0 +1,36 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class ABC090D {
+
+    public static void main(String[] args) {
+        int n = nextInt();
+        int k = nextInt();
+        long ans = 0;
+        for (int b = k+1; b <= n; b++) {
+            ans += (long) Math.max(0, n/b) * (b-k);
+            ans += Math.max(0, n%b-k+1);
+            ans -= k==0?1:0;
+        }
+        out.println(ans);
+        out.flush();
+    }
+
+    static PrintWriter out = new PrintWriter(System.out);
+    static Scanner scanner = new Scanner(System.in);
+    static String next() { return scanner.next(); }
+    static int nextInt() { return Integer.parseInt(next()); }
+    static long nextLong() { return Long.parseLong(next()); }
+    static double nextDouble() { return Double.parseDouble(next()); }
+    static int[] nextIntArray(int n) {
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) { array[i] = nextInt(); }
+        return array;
+    }
+    static long[] nextLongArray(int n) {
+        long[] array = new long[n];
+        for (int i = 0; i < n; i++) { array[i] = nextLong(); }
+        return array;
+    }
+
+}
