39e6cd597399b40a654bc0b7801b31df3c889249
sanopi <36667265+sanopi@users.noreply.github.com>
2022-03-07 11:30:04 +0900

    AGC033A | 想定解（BFS）

--- agc0/agc03/agc033/AGC033A.java
+++ agc0/agc03/agc033/AGC033A.java
@@ -1,5 +1,7 @@
 import java.io.PrintWriter;
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class AGC033A {
@@ -18,6 +20,55 @@ public class AGC033A {
         for (int i = 0; i < h; i++) {
             g[i] = next().toCharArray();
         }
+
+        int ans;
+//        ans = solve1();
+        ans = solve2();
+
+
+        out.println(ans);
+        out.flush();
+    }
+
+    private static int solve2() {
+        Queue<Pair> q = new ArrayDeque<>();
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                if (g[i][j] == '#') {
+                    q.add(new Pair(i, j, 0));
+                }
+            }
+        }
+
+        int ans = 0;
+        while (!q.isEmpty()) {
+            Pair current = q.poll();
+            for (int i = 0; i < 4; i++) {
+                int x = current.x+nx[i];
+                int y = current.y+ny[i];
+                if (0 <= x && x < h && 0 <= y && y < w) {
+                    if (g[x][y] == '#') continue;
+                    g[x][y] = '#';
+                    ans = Math.max(ans, current.count+1);
+                    q.add(new Pair(x, y, current.count+1));
+                }
+            }
+        }
+        return ans;
+    }
+
+    private static class Pair {
+        int x;
+        int y;
+        int count;
+        public Pair(int x, int y, int count) {
+            this.x = x;
+            this.y = y;
+            this.count = count;
+        }
+    }
+
+    private static int solve1() {
         diff = new int[h][w];
         for (int i = 0; i < h; i++) {
             for (int j = 0; j < w; j++) {
@@ -53,12 +104,9 @@ public class AGC033A {
                 );
             }
         }
-
-        int ans = Arrays.stream(diff)
+        return Arrays.stream(diff)
             .flatMapToInt(ints -> Arrays.stream(ints))
             .max().getAsInt();
-        out.println(ans);
-        out.flush();
     }
 
     static PrintWriter out = new PrintWriter(System.out);
