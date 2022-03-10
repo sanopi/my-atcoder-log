7a3e24aeb66a8a10a5006569798137a878959866
sanopi <36667265+sanopi@users.noreply.github.com>
2022-03-10 10:48:17 +0900

    ABC121D | 想定解

--- abc1/abc12/abc121/ABC121D.java
+++ abc1/abc12/abc121/ABC121D.java
@@ -6,16 +6,30 @@ public class ABC121D {
     public static void main(String[] args) {
         long a = nextLong();
         long b = nextLong();
+        long ans;
+//        ans = solve1(a, b);
+        ans = solve2(a, b);
+        out.println(ans);
+        out.flush();
+    }
+    private static long solve2(long a, long b) {
+        return f0(a-1) ^ f0(b);
+    }
+
+    private static long f0(long n) {
+        return ((n+1)/2)%2 ^ (n%2==0?n:0);
+    }
+
+    private static long solve1(long a, long b) {
         long ans = 0;
         for (int i = 0; i < 63; i++) {
             long expi = 1L << (i);
             long expi1 = 1L << (i + 1);
-            long countA = a/expi1*expi + Math.max(0, a % expi1 - expi);
-            long countB = (b+1)/expi1*expi + Math.max(0, (b+1) % expi1 - expi);
+            long countA = a /expi1*expi + Math.max(0, a % expi1 - expi);
+            long countB = (b +1)/expi1*expi + Math.max(0, (b +1) % expi1 - expi);
             ans += expi * ((countB-countA)%2);
         }
-        out.println(ans);
-        out.flush();
+        return ans;
     }
 
     static PrintWriter out = new PrintWriter(System.out);
