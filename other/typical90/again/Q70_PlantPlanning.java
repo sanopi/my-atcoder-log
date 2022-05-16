c564398f6b963434a61b9527256218bf7ece78a1
sanopi <36667265+sanopi@users.noreply.github.com>
2022-05-16 10:01:47 +0900

    Q70_PlantPlanning | 解き直し

---
+++ other/typical90/again/Q70_PlantPlanning.java
@@ -0,0 +1,45 @@
+package again;
+
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Q70_PlantPlanning {
+
+    public static void main(String[] args) {
+        int n = nextInt();
+        int[] x = new int[n];
+        int[] y = new int[n];
+        for (int i = 0; i < n; i++) {
+            x[i] = nextInt();
+            y[i] = nextInt();
+        }
+        Arrays.sort(x);
+        Arrays.sort(y);
+        long ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans += Math.abs(x[i]-x[n/2]);
+            ans += Math.abs(y[i]-y[n/2]);
+        }
+        out.println(ans);
+        out.flush();
+    }
+
+    static PrintWriter out = new PrintWriter(System.out);
+    static Scanner scanner = new Scanner(System.in);
+    static String next() { return scanner.next(); }
+    static int nextInt() { return Integer.parseInt(next()); }
+    static long nextLong() { return Long.parseLong(next()); }
+    static double nextDouble() { return Double.parseDouble(next()); }
+    static int[] nextIntArray(int n) {
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) { array[i] = nextInt(); }
+        return array;
+    }
+    static long[] nextLongArray(int n) {
+        long[] array = new long[n];
+        for (int i = 0; i < n; i++) { array[i] = nextLong(); }
+        return array;
+    }
+
+}
