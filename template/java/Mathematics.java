ef0bd6ee26a265b1efbe90d8e58b9bdeb1eba6a7
sanopi <36667265+sanopi@users.noreply.github.com>
2022-06-04 23:20:08 +0900

    ライブラリ修正

--- template/java/Mathematics.java
+++ template/java/Mathematics.java
@@ -40,6 +40,7 @@ public class Mathematics {
     }
 
     private static long gcd(long a, long b) {
+        if (b == 0) return a;
         if (a % b == 0) { return b; }
         return gcd(b, a % b);
     }
