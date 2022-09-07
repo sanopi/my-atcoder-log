f6e68f2d3524a940483ca7c005cf11727dce278f
sanopi <36667265+sanopi@users.noreply.github.com>
2022-09-07 10:11:46 +0900

    テンプレートの数字入力を地味に高速化

--- template/java/JavaTemplate.java
+++ template/java/JavaTemplate.java
@@ -11,8 +11,26 @@ public class JavaTemplate {
     static PrintWriter out = new PrintWriter(System.out);
     static Scanner scanner = new Scanner(System.in);
     static String next() { return scanner.next(); }
-    static int nextInt() { return Integer.parseInt(next()); }
-    static long nextLong() { return Long.parseLong(next()); }
+    static int nextInt() {
+        int res = 0;
+        char[] chars = next().toCharArray();
+        boolean minus = chars[0] == '-';
+        int start = minus?1:0;
+        for (int i = start; i < chars.length; i++) {
+            res = res*10 + (chars[i]-'0');
+        }
+        return minus?-res:res;
+    }
+    static long nextLong() {
+        long res = 0;
+        char[] chars = next().toCharArray();
+        boolean minus = chars[0] == '-';
+        int start = minus?1:0;
+        for (int i = start; i < chars.length; i++) {
+            res = res*10 + (chars[i]-'0');
+        }
+        return minus?-res:res;
+    }
     static double nextDouble() { return Double.parseDouble(next()); }
     static int[] nextIntArray(int n) {
         int[] array = new int[n];
