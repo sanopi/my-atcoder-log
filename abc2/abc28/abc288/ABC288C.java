274559cf33d56043bc4ed1e30e077390af398c69
sanopi <36667265+sanopi@users.noreply.github.com>
2023-02-09 22:31:44 +0900

    ABC288

---
+++ abc2/abc28/abc288/ABC288C.java
@@ -0,0 +1,117 @@
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ABC288C {
+
+    public static void main(String[] args) {
+        int n = nextInt();
+        UnionFind uf = new UnionFind(n);
+        int ans = 0;
+        int m = nextInt();
+        for (int i = 0; i < m; i++) {
+            int a = nextInt()-1;
+            int b = nextInt()-1;
+            if (uf.isUnited(a,b)) {
+                ans++;
+                continue;
+            }
+            uf.unite(a, b);
+        }
+        out.println(ans);
+        out.flush();
+    }
+
+    static PrintWriter out = new PrintWriter(System.out);
+    static Scanner scanner = new Scanner(System.in);
+    static String next() { return scanner.next(); }
+    static int nextInt() {
+        int res = 0;
+        char[] chars = next().toCharArray();
+        boolean minus = chars[0] == '-';
+        int start = minus?1:0;
+        for (int i = start; i < chars.length; i++) {
+            res = res*10 + (chars[i]-'0');
+        }
+        return minus?-res:res;
+    }
+    static long nextLong() {
+        long res = 0;
+        char[] chars = next().toCharArray();
+        boolean minus = chars[0] == '-';
+        int start = minus?1:0;
+        for (int i = start; i < chars.length; i++) {
+            res = res*10 + (chars[i]-'0');
+        }
+        return minus?-res:res;
+    }
+    static double nextDouble() { return Double.parseDouble(next()); }
+    static int[] nextIntArray(int n) {
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) { array[i] = nextInt(); }
+        return array;
+    }
+    static long[] nextLongArray(int n) {
+        long[] array = new long[n];
+        for (int i = 0; i < n; i++) { array[i] = nextLong(); }
+        return array;
+    }
+
+    private static class UnionFind {
+        int[] parents; // 親（根）の情報を持つ
+        int[] ranks; // 深さの最大値の情報を持つ（複雑度と同じくらいに考えておく。）
+        int[] sizes;
+
+        UnionFind(int n) {
+            parents = new int[n];
+            ranks = new int[n];
+            for (int i = 0; i < n; i++) {
+                parents[i] = i;
+                ranks[i] = 0;
+            }
+            sizes = new int[n];
+            Arrays.fill(sizes, 1);
+        }
+
+        private int find(int x) {
+            int parent = parents[x];
+            if (x == parent) {
+                return x;
+            }
+            parents[x] = find(parent); // 圧縮（木の繋ぎ直し）
+            return parents[x];
+        }
+
+        private boolean isUnited(int x, int y) {
+            return find(x) == find(y);
+        }
+
+        private int getSize(int x) {
+            return sizes[find(x)];
+        }
+
+        private void unite(int x, int y) {
+            int xParent = find(x);
+            int yParent = find(y);
+            if (xParent == yParent) {
+                return;
+            }
+
+            int xRank = ranks[xParent];
+            int yRank = ranks[yParent];
+            if (xRank < yRank) {
+                parents[xParent] = yParent;
+                sizes[yParent] += sizes[xParent];
+            } else if (yRank < xRank) {
+                parents[yParent] = xParent;
+                sizes[xParent] += sizes[yParent];
+            } else { // xRank == yRank
+                parents[xParent] = yParent;
+                ranks[xParent]++;
+                sizes[yParent] += sizes[xParent];
+            }
+        }
+    }
+
+
+}
