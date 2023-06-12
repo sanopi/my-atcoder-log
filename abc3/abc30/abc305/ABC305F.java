a11eed54dfbb09ff20d2998b2a878dec81ebb3af
sanopi <36667265+sanopi@users.noreply.github.com>
2023-06-12 10:08:56 +0900

    ABC305F

---
+++ abc3/abc30/abc305/ABC305F.java
@@ -0,0 +1,80 @@
+import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class ABC305F {
+
+    private static final Set<Integer> done = new HashSet<>(100);
+
+    public static void main(String[] args) {
+        int n = nextInt();
+        int m = nextInt();
+        done.add(1);
+        solve(1, 0);
+    }
+
+    private static boolean solve(int current, int prev) {
+        String ks = next();
+        if (ks.equals("OK")) {
+            return true;
+        }
+        int k = Integer.parseInt(ks);
+        int[] v = new int[k];
+        for (int i = 0; i < k; i++) {
+            v[i] = nextInt();
+        }
+        for (int i = 0; i < k; i++) {
+            int next = v[i];
+            if (done.contains(next)) continue;
+            done.add(next);
+            System.out.println(next);
+            if (solve(next, current)) {
+                return true;
+            }
+            int kk = nextInt();
+            for (int i1 = 0; i1 < kk; i1++) {
+                nextInt();
+            }
+        }
+
+        System.out.println(prev);
+        return false;
+    }
+
+    static PrintWriter out = new PrintWriter(System.out);
+    static Scanner scanner = new Scanner(System.in);
+    static String next() { return scanner.next(); }
+    static int nextInt() {
+        int res = 0;
+        char[] chars = next().toCharArray();
+        boolean minus = chars[0] == '-';
+        int start = minus?1:0;
+        for (int i = start; i < chars.length; i++) {
+            res = res*10 + (chars[i]-'0');
+        }
+        return minus?-res:res;
+    }
+    static long nextLong() {
+        long res = 0;
+        char[] chars = next().toCharArray();
+        boolean minus = chars[0] == '-';
+        int start = minus?1:0;
+        for (int i = start; i < chars.length; i++) {
+            res = res*10 + (chars[i]-'0');
+        }
+        return minus?-res:res;
+    }
+    static double nextDouble() { return Double.parseDouble(next()); }
+    static int[] nextIntArray(int n) {
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) { array[i] = nextInt(); }
+        return array;
+    }
+    static long[] nextLongArray(int n) {
+        long[] array = new long[n];
+        for (int i = 0; i < n; i++) { array[i] = nextLong(); }
+        return array;
+    }
+
+}
